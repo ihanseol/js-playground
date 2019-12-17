708c9903fe98356ba3e0287baf80c2401bdb232b
ihanseol <hansel33@naver.com>
2019-12-17 18:02:06 +0900

    add javascript example

---
+++ 01_javascript/03_test/hello.js
@@ -0,0 +1,10 @@
+alert('hello');
+
+for(let i = 1; i<=10; i++ ) {
+    console.log(`${i} is running ...`);
+}
+
+
+
+
+
---
+++ 01_javascript/03_test/script.js
@@ -0,0 +1,13 @@
+function test() {
+
+    alert("hello world ...");
+
+    debugger;
+    for(let i = 1; i<=10; i++ ) {
+        console.log(`${i} is running ...`);
+    }
+
+    return 0;
+
+}
+
